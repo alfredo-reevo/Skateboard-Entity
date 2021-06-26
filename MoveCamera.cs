b13e9bae6c4e228e18efffc53743321a534d4cf5
Alfie Reeves <reevesalfie@gmail.com>
2021-06-26 17:10:44 +0100

    Typical player movement scripts

---
+++ MoveCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
